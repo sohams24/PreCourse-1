583cab25de2054bd2d87cb8336900b8cc7d25fc2
sohams24 <sohamsatam537@gmail.com>
2024-05-12 19:26:52 -0700

    Exercise 2 complete

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,28 @@
 
 class Node:
-    def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
 class Stack:
-    def __init__(self):
-        
-    def push(self, data):
-        
-    def pop(self):
-        
+  def __init__(self):
+    self.__head = None
+    self.__size = 0
+
+  def push(self, data):
+    newNode = Node(data)
+    newNode.next = self.__head
+    self.__head = newNode
+    self.__size += 1
+
+  def pop(self):
+    if self.__size == 0:
+      return None
+    popedNode = self.__head
+    self.__head = popedNode.next
+    self.__size -= 1
+    return popedNode.data
+
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
